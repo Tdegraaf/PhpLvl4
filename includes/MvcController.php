8f1cf9fa7343999e1d73c6daf5755e9cd9b0bb7c
T. de Graaf <107231@student.drenthecollege.nl>
2024-03-26 12:39:03 +0100

    IMPL - Base file structure   Added base file structure.   includes   stylesheets   and index.php

---
+++ includes/MvcController.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+/**
+ * Description of MvcController
+ *
+ * @author thier
+ */
+class MvcController {
+    //put your code here
+}
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>MVC Structure Assignment</title>
+        <link rel="stylesheet" type="text/css" href="stylesheets/Theme.css"
+    </head>
+    <body>
+        <?php
+        include 'includes/MvcController.php';
+        include 'includes/MvcModel.php';
+        include 'includes/MvcView.php';
+        ?>
+    </body>
+</html>
